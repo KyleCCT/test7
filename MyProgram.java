e069f6b490d0d747c3ff11ff4aa73f7bdf3d9949
kylecct <kyle@cct.ie>
2017-10-20 12:51:22 +0100

    first code

---
+++ MyProgram.java
@@ -0,0 +1,40 @@
+/*
+----------------------------------------
+Hello there this is a multiline block
+
+Author: Kyle Goslin
+Date: 15/9/17
+
+Version: 1.0
+
+----------------------------------------
+*/
+
+class MyProgram {
+
+
+static String personsName = "Kyle";
+static String personsSecondName = "Goslin";
+
+
+static String college = "CCT";
+
+static int building = 30;
+
+static double cost = 45.55;
+
+static boolean isWorking = true;
+
+
+// This is the main method
+ public static void main(String[]args){
+
+  // This is a little block that says hello 
+  System.out.println("Hello there....." + personsName + " " + personsSecondName);
+  
+  System.out.println("Hi");
+  System.out.println("....... hello");
+ 
+ }
+
+}
